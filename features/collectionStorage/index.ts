4622872c89305bf27d0db5837456bf3318720965
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-11 14:31:01 +0500

    Update index.ts

--- features/collectionStorage/index.ts
+++ features/collectionStorage/index.ts
@@ -1,15 +1,16 @@
 import { collectionStorage } from "./utils/collectionStorage";
+
 export * from "./utils/getCollections";
 
 export { default as getCollections } from "./utils/getCollections";
-export { default as resoveConflict } from "./utils/resolveConflict";
+export { default as resolveConflict } from "./utils/resolveConflict";
 export { default as saveCollections } from "./utils/saveCollections";
 export { default as setCloudStorage } from "./utils/setCloudStorage";
 export { default as clearGraphicsStorage } from "./utils/clearGraphics";
-
 export { default as thumbnailCaptureEnabled } from "./utils/thumbnailCaptureEnabled";
 
-export const collectionCount = collectionStorage.count;
-export const graphics = collectionStorage.graphics;
-
-export const cloudDisabled = collectionStorage.disableCloud;
+export const {
+	count: collectionCount,
+	graphics,
+	disableCloud: cloudDisabled
+} = collectionStorage;
