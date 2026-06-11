e87d532357d04bb852d3ff6383ff7c6c29e9d48b
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-11 14:29:23 +0500

    Update analytics.ts

--- features/analytics/utils/analytics.ts
+++ features/analytics/utils/analytics.ts
@@ -1,12 +1,24 @@
 import { createAnalytics } from "@wxt-dev/analytics";
 import { googleAnalytics4 } from "@wxt-dev/analytics/providers/google-analytics-4";
 
+const measurementId = import.meta.env.WXT_GA4_MEASUREMENT_ID;
+const apiSecret = import.meta.env.WXT_GA4_API_SECRET;
+
+if (!measurementId)
+{
+	throw new Error("Missing WXT_GA4_MEASUREMENT_ID environment variable.");
+}
+
+if (!apiSecret)
+{
+	throw new Error("Missing WXT_GA4_API_SECRET environment variable.");
+}
+
 export const analytics = createAnalytics({
-	providers:
-		[
-			googleAnalytics4({
-				apiSecret: import.meta.env.WXT_GA4_API_SECRET,
-				measurementId: import.meta.env.WXT_GA4_MEASUREMENT_ID
-			})
-		]
+	providers: [
+		googleAnalytics4({
+			measurementId,
+			apiSecret
+		})
+	]
 });
