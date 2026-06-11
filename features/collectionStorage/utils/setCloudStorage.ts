990a35252d4c3a02220681ec4acf9d6d79d4ffae
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-11 14:36:34 +0500

    Update setCloudStorage.ts

--- features/collectionStorage/utils/setCloudStorage.ts
+++ features/collectionStorage/utils/setCloudStorage.ts
@@ -1,20 +1,36 @@
 import { sendMessage } from "@/utils/messaging";
+
 import { collectionStorage } from "./collectionStorage";
 import saveCollectionsToCloud from "./saveCollectionsToCloud";
 
-export default async function setCloudStorage(enable: boolean): Promise<void>
+export default async function setCloudStorage(
+	enable: boolean
+): Promise<void>
 {
+	await collectionStorage.disableCloud.setValue(
+		!enable
+	);
+
 	if (enable)
 	{
-		await collectionStorage.disableCloud.setValue(false);
-		const collections = await collectionStorage.localCollections.getValue();
-		const lastUpdated = await collectionStorage.localLastUpdated.getValue();
-		await saveCollectionsToCloud(collections, lastUpdated);
+		const [collections, lastUpdated] =
+			await Promise.all([
+				collectionStorage.localCollections.getValue(),
+				collectionStorage.localLastUpdated.getValue()
+			]);
+
+		await saveCollectionsToCloud(
+			collections,
+			lastUpdated
+		);
+
+		return;
 	}
-	else
-	{
-		await collectionStorage.disableCloud.setValue(true);
-		await saveCollectionsToCloud([], 0);
-		await sendMessage("refreshCollections", undefined);
-	}
+
+	await saveCollectionsToCloud([], 0);
+
+	await sendMessage(
+		"refreshCollections",
+		undefined
+	);
 }
